4134b5453b3d9bcc3beadb365494d44466522cd9
Bushra Shaikh <bushra.sk@siesgst.ac.in>
2022-08-12 12:11:03 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class sum  
+{  
+public static void main(String args[])  
+{  
+int x, y, sum;  
+x=2;
+y=3;
+ 
+/*x = Integer.parseInt(System.getenv("a"));  
+
+//y = Integer.parseInt(System.getenv("b"));  
+
+
+Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        x = Integer.parseInt(System.getenv(sc.nextInt()));
+        
+        System.out.println("Enter Second Number: ");
+        y = Integer.parseInt(System.getenv(sc.nextInt()));
+        
+        sc.close();
+
+*/
+sum = sum(x, y);  
+System.out.println("The sum of two numbers x and y is: " + sum);  
+}  
+//method that calculates the sum  
+public static int sum(int a, int b)  
+{  
+int sum = a + b;  
+return sum;  
+}   
+}  
